f05f945d164b3b2c9e959a92b1c878600bc3c988
Jacques-Olivier Haché <jacquesolivier.hache@gmail.com>
2014-03-19 12:39:05 +0800

    ENH: injected the plugin object in the tests that are already functional with the plugin. Did minor changes is some tests (performance does not exist in every browser), imported our adapter.js

--- base/adapter.js
+++ base/adapter.js
@@ -4,24 +4,24 @@ var attachMediaStream = null;
 var reattachMediaStream = null;
 var webrtcDetectedBrowser = null;
 var webrtcDetectedVersion = null;
+var TemPluginActivated = null;
+
+  //DEBUG
+  function plugin0()
+  {
+      return document.getElementById('plugin0');
+  }
+  plugin = plugin0;
+
+  if (plugin())
+    plugin().setLogFunction(console);
 
 function trace(text) {
   // This function is used for logging.
   if (text[text.length - 1] == '\n') {
     text = text.substring(0, text.length - 1);
   }
-  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
-}
-function maybeFixConfiguration(pcConfig) {
-  if (pcConfig == null) {
-    return;
-  }
-  for (var i = 0; i < pcConfig.iceServers.length; i++) {
-    if (pcConfig.iceServers[i].hasOwnProperty('urls')){
-      pcConfig.iceServers[i]['url'] = pcConfig.iceServers[i]['urls'];
-      delete pcConfig.iceServers[i]['urls'];
-    }
-  }
+  console.log(/*(performance.now() / 1000).toFixed(3) + ": " +*/ text); // some browsers don't have a performance object
 }
 
 if (navigator.mozGetUserMedia) {
@@ -33,11 +33,7 @@ if (navigator.mozGetUserMedia) {
            parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1], 10);
 
   // The RTCPeerConnection object.
-  var RTCPeerConnection = function(pcConfig, pcConstraints) {
-    // .urls is not supported in FF yet.
-    maybeFixConfiguration(pcConfig);
-    return new mozRTCPeerConnection(pcConfig, pcConstraints);
-  }
+  RTCPeerConnection = mozRTCPeerConnection;
 
   // The RTCSessionDescription object.
   RTCSessionDescription = mozRTCSessionDescription;
@@ -65,34 +61,20 @@ if (navigator.mozGetUserMedia) {
         // Return null for createIceServer if transport=tcp.
         if (turn_url_parts.length === 1 ||
             turn_url_parts[1].indexOf('transport=udp') === 0) {
-          iceServer = {'url': turn_url_parts[0],
-                       'credential': password,
-                       'username': username};
+          iceServer = { 'url': turn_url_parts[0],
+                        'credential': password,
+                        'username': username };
         }
       } else {
         // FF 27 and above supports transport parameters in TURN url,
         // So passing in the full url to create iceServer.
-        iceServer = {'url': url,
-                     'credential': password,
-                     'username': username};
+        iceServer = { 'url': url,
+                      'credential': password,
+                      'username': username };
       }
     }
     return iceServer;
   };
-
-  createIceServers = function(urls, username, password) {
-    var iceServers = [];
-    // Use .url for FireFox.
-    for (i = 0; i < urls.length; i++) {
-      var iceServer = createIceServer(urls[i],
-                                      username,
-                                      password);
-      if (iceServer !== null) {
-        iceServers.push(iceServer);
-      }
-    }
-    return iceServers;
-  }
 
   // Attach a media stream to an element.
   attachMediaStream = function(element, stream) {
@@ -119,14 +101,16 @@ if (navigator.mozGetUserMedia) {
       return [];
     };
   }
-} else if (navigator.webkitGetUserMedia) {
+} else if (navigator.webkitGetUserMedia) { //////////////////////////////////////////////////////////////////////////////////////
   console.log("This appears to be Chrome");
 
   webrtcDetectedBrowser = "chrome";
   webrtcDetectedVersion =
          parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2], 10);
 
-  // Creates iceServer from the url for Chrome M33 and earlier.
+  TemPluginActivated = plugin() && plugin().valid;
+
+  // Creates iceServer from the url for Chrome.
   createIceServer = function(url, username, password) {
     var iceServer = null;
     var url_parts = url.split(':');
@@ -135,64 +119,130 @@ if (navigator.mozGetUserMedia) {
       iceServer = { 'url': url };
     } else if (url_parts[0].indexOf('turn') === 0) {
       // Chrome M28 & above uses below TURN format.
-      iceServer = {'url': url,
-                   'credential': password,
-                   'username': username};
+      iceServer = { 'url': url,
+                    'credential': password,
+                    'username': username };
     }
     return iceServer;
   };
 
-  // Creates iceServers from the urls for Chrome M34 and above.
-  createIceServers = function(urls, username, password) {
-    var iceServers = [];
-    if (webrtcDetectedVersion >= 34) {
-      // .urls is supported since Chrome M34.
-      iceServers = {'urls': urls,
-                    'credential': password,
-                    'username': username };
-    } else {
-      for (i = 0; i < urls.length; i++) {
-        var iceServer = createIceServer(urls[i],
-                                        username,
-                                        password);
-        if (iceServer !== null) {
-          iceServers.push(iceServer);
-        }
-      }
-    }
-    return iceServers;
-  };
-
-  // The RTCPeerConnection object.
-  var RTCPeerConnection = function(pcConfig, pcConstraints) {
-    // .urls is supported since Chrome M34.
-    if (webrtcDetectedVersion < 34) {
-      maybeFixConfiguration(pcConfig);
-    }
-    return new webkitRTCPeerConnection(pcConfig, pcConstraints);
+  // The RTCSessionDescription object.
+  RTCSessionDescription = function(info) {
+    return plugin().ConstructSessionDescription(info.type, info.sdp);
   }
 
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+  RTCPeerConnection = function(servers, constraints) {
+    var iceServers = servers ? servers.iceServers : null;
+    var mandatory = constraints ? constraints.mandatory : null;
+    var optional = constraints ? constraints.optional : null;
+    return plugin().PeerConnection(iceServers, mandatory, optional);
+  }
+
+  if (!MediaStreamTrack)
+    MediaStreamTrack = {};
+  MediaStreamTrack.getSources = function(callback) {
+    plugin().GetSources(callback);
+  };
+
+  function getUserMedia_w(constraints, successCallback, failureCallback) {
+    plugin().getUserMedia(constraints, successCallback, failureCallback);
+  };
+
+  getUserMedia = getUserMedia_w; //plugin().getUserMedia;
   navigator.getUserMedia = getUserMedia;
 
   // Attach a media stream to an element.
   attachMediaStream = function(element, stream) {
-    if (typeof element.srcObject !== 'undefined') {
-      element.srcObject = stream;
-    } else if (typeof element.mozSrcObject !== 'undefined') {
-      element.mozSrcObject = stream;
-    } else if (typeof element.src !== 'undefined') {
-      element.src = URL.createObjectURL(stream);
-    } else {
-      console.log('Error attaching stream to element.');
+    if (TemPluginActivated)
+      plugin().renderStream(stream);
+    else {
+      if (typeof element.srcObject !== 'undefined') {
+        element.srcObject = stream;
+      } else if (typeof element.mozSrcObject !== 'undefined') {
+        element.mozSrcObject = stream;
+      } else if (typeof element.src !== 'undefined') {
+        element.src = URL.createObjectURL(stream);
+      } else {
+        console.log('Error attaching stream to element.');
+      }
     }
   };
 
   reattachMediaStream = function(to, from) {
     to.src = from.src;
   };
-} else {
+
+  RTCIceCandidate = function(candidate) {
+    return plugin().ConstructIceCandidate(candidate.sdpMid, candidate.sdpMLineIndex, candidate.candidate);
+  };
+
+} else if (navigator.userAgent.indexOf("Safari")) { ////////////////////////////////////////////////////////////////////////
+  console.log("This appears to be Safari");
+
+  TemPluginActivated = plugin() && plugin().valid;
+
+  createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var url_parts = url.split(':');
+    if (url_parts[0].indexOf('stun') === 0) {
+      // Create iceServer with stun url.
+      iceServer = { 'url': url, 'hasCredentials': false};
+    } else if (url_parts[0].indexOf('turn') === 0) {
+      iceServer = { 'url': url,
+                    'hasCredentials': true,
+                    'credential': password,
+                    'username': username };
+    }
+    return iceServer;
+  };
+
+  // The RTCSessionDescription object.
+  RTCSessionDescription = function(info) {
+    return plugin().ConstructSessionDescription(info.type, info.sdp);
+  }
+
+  // PEER CONNECTION
+  RTCPeerConnection = function(servers, constraints) {
+    var iceServers = servers ? servers.iceServers : null;
+    var mandatory = (constraints && constraints.mandatory) ? constraints.mandatory : null;
+    var optional = (constraints && constraints.optional) ? constraints.optional : null;
+    return plugin().PeerConnection(iceServers, mandatory, optional);
+  }
+
+  MediaStreamTrack = {};
+  MediaStreamTrack.getSources = function(callback) {
+    plugin().GetSources(callback);
+  };
+
+  function getUserMedia_w(constraints, successCallback, failureCallback) {
+    plugin().getUserMedia(constraints, successCallback, failureCallback);
+  };
+
+  getUserMedia = getUserMedia_w; //plugin().getUserMedia;
+  navigator.getUserMedia = getUserMedia;
+
+  RTCIceCandidate = function(candidate) {
+    return plugin().ConstructIceCandidate(candidate.sdpMid, candidate.sdpMLineIndex, candidate.candidate);
+  };
+
+  // Attach a media stream to an element.
+  attachMediaStream = function(element, stream) {
+    if (TemPluginActivated)
+      plugin().renderStream(stream);
+    else {
+      if (typeof element.srcObject !== 'undefined') {
+        element.srcObject = stream;
+      } else if (typeof element.mozSrcObject !== 'undefined') {
+        element.mozSrcObject = stream;
+      } else if (typeof element.src !== 'undefined') {
+        element.src = URL.createObjectURL(stream);
+      } else {
+        console.log('Error attaching stream to element.');
+      }
+    }
+  };
+
+} 
+else {
   console.log("Browser does not appear to be WebRTC-capable");
 }
